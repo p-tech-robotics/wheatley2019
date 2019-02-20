4e7521fcd41e8ea48ff2692e61ae99bd112f044d
kemfic <19ficici.kemal@gmail.com>
2019-02-20 12:53:14 -0500

    removed robot.py for newer code

--- rio-code/robot.py
+++ rio-code/robot.py
@@ -1,49 +1,29 @@
-#!/usr/bin/env python3
-"""
-This is a demo program showing the use of the RobotDrive class,
-specifically it contains the code necessary to operate a robot with
-tank drive.
-"""
 import wpilib
-from wpilib.drive import DifferentialDrive
+from magicbot import MagicRobot
+
+#from components.component1 import Component1
+
+from components import Wrist, Intake, Popper
+
+clas MyRobot(MagicRobot):
+  #component1 = Component1
+
+  k_constant = 1
+
+  def createObjects(self):
+    """Initialize all motors and sensors"""
+
+    self.component1_motor = wpilib.Spark(1)
+    self.component2
 
 
-class MyRobot(wpilib.TimedRobot):
-  def robotInit(self):
-    """Robot initialization function"""
-
-    # object that handles basic drive operations
-    self.cargoMotor = wpilib.Spark(5)
-
-    self.frontLeftMotor = wpilib.Spark(2)
-    self.rearLeftMotor = wpilib.Spark(4)
-    self.frontRightMotor = wpilib.Spark(3)
-    self.rearRightMotor = wpilib.Spark(0)
-
-    self.left = wpilib.SpeedControllerGroup(self.frontLeftMotor, self.rearLeftMotor)
-    self.right = wpilib.SpeedControllerGroup(
-      self.frontRightMotor, self.rearRightMotor
-    )
-
-    self.myRobot = wpilib.drive.DifferentialDrive(self.left, self.right)
-    # joysticks 1 & 2 on the driver station
-    self.stick = wpilib.Joystick(0)
-    wpilib.CameraServer.launch()
-    self.joystick = wpilib.Joystick(1)
-    self.timer = wpilib.Timer()
-  def autonomousInit(self):
-    """Runs once each time bot enters auto mode"""
-    self.timer.reset()
-    self.timer.start()
+  def teleopInit(self):
+    """called when teleop starts"""
 
   def teleopPeriodic(self):
-    """Runs the motors with tank steering"""
-    # exponential response
-    # self.myRobot.arcadeDrive(self.stick.getRawAxis(1)**3, self.stick.getRawAxis(4)**3)
-    # Linear Response
-    self.cargoMotor.set(self.joystick.getY())
-    self.myRobot.arcadeDrive(self.stick.getRawAxis(1), self.stick.getRawAxis(4))
+    """called on each iteration of teleop loop"""
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
   wpilib.run(MyRobot)
+
