5ead51def438bf6cb88e8dcfc126999dab968632
kemfic <19ficici.kemal@gmail.com>
2019-02-20 17:42:48 -0500

    started doing drivetrain

--- rio-code/components/drive.py
+++ rio-code/components/drive.py
@@ -5,6 +5,27 @@ class Drivetrain:
   """
   Robot Drivetrain
   """
+  def __init__(self,
+      l_f_port=0,
+      l_r_port=1,
+      r_f_port=2,
+      r_r_port=3):
+    """
+    Initialize ports for bot
+
+    var naming scheme: (side)_(position)_(object), ex: l_f_port is left_front_port
+    """
+
+    # Motors on Left Side
+    l_f_motor = Spark(l_f_port)
+    f_r_motor = Spark(l_r_port)
+
+    # Motors on Right Side
+    r_f_motor = Spark(r_f_port)
+    r_r_motor = Spark(r_r_port)
+
+    # Motor groups
+
 
 
 
