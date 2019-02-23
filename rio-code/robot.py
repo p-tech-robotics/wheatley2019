870d2c14146cb1dbe251e257cf26105194e2e752
kemfic <19ficici.kemal@gmail.com>
2019-02-23 15:14:57 -0500

    some code runs for now

--- rio-code/robot.py
+++ rio-code/robot.py
@@ -1,7 +1,7 @@
 import wpilib
 from wpilib import TimedRobot, Timer, Joystick, CameraServer, PowerDistributionPanel, DriverStation
 
-from components import drive, wrist, intake, popper, encoders, imu, statemachine
+from components import drive, wrist, intake, popper, encoders, imu#, statemachine
 
 class Wheatley(TimedRobot):
   def robotInit(self):
