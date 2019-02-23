8e8f6de6dc6ed45c04b792a9461c87f2bfbed4eb
kemfic <19ficici.kemal@gmail.com>
2019-02-23 15:01:51 -0500

    added field ASCII drawing

--- rio-code/components/statemachine.py
+++ rio-code/components/statemachine.py
@@ -2,6 +2,22 @@ import wpilib
 
 from robotpy_ext.autonomous import StatefulAutonomous
 
+"""
+\ r3l          | c3l       c3r |          r3r /
+ |             | c2l       c2r |             |
+ |  r2l        | c1l       c1r |       r2r   |
+/ r1l          |   c0l   c0r   |          r1r \
+               |-------|-------|
+
+ ls_l                                      ls_r
+---|---___________________________________--|--
+
+"""
+
+
+
+
+
 class StateMachine(StatefulAutonomous):
   MODE_NAME = "Drive Forward"
 
@@ -13,7 +29,12 @@ class StateMachine(StatefulAutonomous):
   def state_method_name(self, initial_call):
     pass
   '''
+  def teleop(self):
 
+  def ls_a(self):
+    """
+    Go to loading station from cargo ship A
+    """
 
 
 
