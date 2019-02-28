7b3de53d3075b79109c44827a928106343b5e41f
kemfic <19ficici.kemal@gmail.com>
2019-02-27 22:53:31 -0500

    fixed merge conflict

--- rio-code/robot.py
+++ rio-code/robot.py
@@ -7,10 +7,6 @@ class Wheatley(TimedRobot):
 <<<<<<< HEAD
   kSpeedLim = 0.8
   kSteerLim = 0.75
-=======
-  kSpeedLimit = 0.8 # to prevent driver from tipping robot
-  kAutoLimit = 0.2 # for testing only, can increase later if things look good
->>>>>>> eca0e92659d91e6e1a791099ccf239d74a943b61
   def robotInit(self):
     """
     Init Robot
@@ -19,13 +15,9 @@ class Wheatley(TimedRobot):
     # Robot Components
     # Constructor params are PWM Ports on the RIO
     self.drive = drive.Drivetrain(1,2,3,4)
-<<<<<<< HEAD
+    
     self.intake = intake.Intake(0)
     self.popper = popper.Popper(0,0)
-=======
-    self.intake = intake.Intake(5)
-    self.popper = popper.Popper(1,0)
->>>>>>> eca0e92659d91e6e1a791099ccf239d74a943b61
 
     self.imu = imu.IMU(2)
     self.encoders = encoders.Encoders()
@@ -36,7 +28,6 @@ class Wheatley(TimedRobot):
 
     self.timer = Timer()
   
-<<<<<<< HEAD
 
   def robotPeriodic(self):
 
@@ -57,8 +48,6 @@ class Wheatley(TimedRobot):
     self.intake.set(self.xbox.getRawAxis(5))
 
 
-=======
->>>>>>> eca0e92659d91e6e1a791099ccf239d74a943b61
   def autonomousInit(self):
     """
     Runs one time whenever the bot enters auto mode
@@ -66,38 +55,6 @@ class Wheatley(TimedRobot):
     self.timer.reset()
     self.timer.start()
 
-  '''
-  def autonomousPeriodic(self):
-    """
-    loops during auto period
-    """
-
-    self.teleopPeriodic() # TODO: remove soon once auto code works
-<<<<<<< HEAD
-  '''
-=======
-  def teleopPeriodic(self):
-    """
-    teleop code
-    """
-    #speed = xbox.getRawAxis(4)
-
-    # triggers for speed, left stick for steer
-    speed= self.kDriveMax*((xbox.getRawAxis(3) - getRawAxis(2))**3) # cubic response
-    steer = self.getRawAxis(0)**3
-
-    self.drive.drive.arcadeDrive(speed,
-                                steer)
-
-
-    # Popper (Left Bumper)
-    self.popper.set(self.xbox.getRawButton(4))
-
-    # Intake Code (Right Stick)
-    self.intake.set(self.xbox.getRawAxis(5))
-
-
->>>>>>> eca0e92659d91e6e1a791099ccf239d74a943b61
 
 if __name__ == '__main__':
   wpilib.run(Wheatley)
