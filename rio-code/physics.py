c590c5b88c957f0b9fdf6725e863d143cd4d61ac
Kemal Ficici <19ficici.kemal@gmail.com>
2019-02-28 18:38:59 -0500

    added sim and physics code

--- rio-code/physics.py
+++ rio-code/physics.py
@@ -8,15 +8,40 @@ class PhysicsEngine(object):
     Simulates drivetrain for pathfinder
   """
 
-  def __init__(self, phys_controller):
+  def __init__(self, physics_controller):
     """
-      :param phys_controller: 'pyfrc.physics.core.PhysicsIntergace' object
+      :param physics_controller: 'pyfrc.physics.core.PhysicsInterface' object
                               to communicate sim effects
     """
 
-    self.phys_controller = phys_controller
-    self.pos = 0
-
+    self.physics_controller = physics_controller
 
     # change these params to fit robot
     bumper_width = 3.25 * units.inch
+
+    # fmt: off
+    self.drivetrain = tankModel.TankModel.theory(
+            motor.cfgs.MOTOR_CFG_CIM,               # motor configuration
+            110 * units.lbs,                        # robot mass
+            10.75,                                  # gear ratio
+            2,                                      # motors per side
+            24* units.inch,                         # dist between wheelbase
+            (28+6.5) * units.inch,                         # robot width
+            (28+6.5) * units.inch.                         # robot length
+            6 * units.inch
+            )
+
+    #fmt: on
+
+  def update_sim(self, hal_data, now, tm_diff):
+    """
+        :param now: current time (float)
+        :param tm_diff: delta_t
+    """
+
+    # simulate the drivetrain
+    lr_motor = hal_data["pwm"][1]["value"]
+    rr_motor = hal_data["pwm"][2]["value"]
+
+    x, y, angle = self.drivetrain.get_distance(lr_motor, rr_motor; tm_diff)
+    self.physics_controller.distance_drive(x, y, angle)
