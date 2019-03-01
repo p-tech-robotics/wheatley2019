dc8f11ce08e3dd402f36e68052a7704e5c3638c2
Kemal Ficici <19ficici.kemal@gmail.com>
2019-03-01 15:19:56 -0500

    added stuff to OI

--- rio-code/oi.py
+++ rio-code/oi.py
@@ -1,5 +1,31 @@
 import wpilib
 from wpilib import shuffleboard
+from wpilib.buttons import JoystickButton
+
 from wpilib.shuffleboard import Shuffleboard as SB
 
-from commands
+from commands.drivebot import DriveBot
+from commands.pop import Pop
+from commands.operateintake import OperateIntake
+from commands.circles import Circles
+
+class OI:
+  def __init__(self, robot):
+
+    self.robot = robot
+    self.xbox = wpilib.Joystick(0)
+
+    self.l_trigger = self.xbox.getRawAxis(2)
+    self.r_trigger = self.xbox.getRawAxis(3)
+
+    self.l_bumper = JoystickButton(self.xbox, 6)
+    self.r_bumper = JoystickButton(self.xbox, 5)
+
+    self.steer = self.xbox.getRawAxis(0)
+    self.speed = self.l_trigger - self.r_trigger
+
+    self.a_button = self.xbox.getRawButton(1)
+    
+    self.l_bumper.whenPressed(Pop(self.robot))
+
+    self.a_button.whenPressed(Circles(self.robot))
